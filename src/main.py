15922662fd2e84db1fe2a0695c08b102414bf03d
Anindita Basu <a.basu@student.uw.edu.pl>
2025-05-14 21:04:31 +0200

    Added Asymmetric Poisson Execution file
    The file run without error via main.py. A_bid and A_ask and k_bid and k_ask are to be given as input to initilise the AsymPoissonOrderExecution Class.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 from src.simulations.arithmetic_brownian import ArithmeticBrownianMotion
 from src.strategies.avellaneda_stoikov_for_abm import AvellanedaStoikovStrategyAbm
 from src.executions.poisson_execution_for_abm import PoissonExecutionForAbm
+from src.executions.Assym_Poisson_Execution import AsymPoissonOrderExecution
 from src.core.inventory_manager import InventoryManager
 from src.core.data_logger import DataLogger
 from src.core.simulation_runner import SimulationRunner
@@ -12,7 +13,8 @@ def main():
     # Initialize components
     market = ArithmeticBrownianMotion(300,100,0.2,42)
     strategy = AvellanedaStoikovStrategyAbm(gamma=1.5, k=1, inventory=0,abm=market)
-    execution = PoissonExecutionForAbm(A=100,cash=0,strategy=strategy)
+    #execution = PoissonExecutionForAbm(A=100,cash=0,strategy=strategy)
+    execution = AsymPoissonOrderExecution(A_bid = 160.0, A_ask = 120.0, k_bid = 1.2, k_ask = 1.8, cash = 0.0, strategy=strategy)
     inventory = InventoryManager(initial_cash=0, initial_inventory=0)
     logger = DataLogger()
 
